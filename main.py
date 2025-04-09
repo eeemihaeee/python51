d08453d9035c454844baec4074e5667a547ea11d
eeemihaeee <69985028+eeemihaeee@users.noreply.github.com>
2025-04-09 18:33:26 +0500

    Введение в Python51

---
+++ main.py
@@ -0,0 +1,33 @@
+#print("Hello world!!!")
+#Функция для печати текста
+# и его вывода в консоль
+# name = input("Введите ваше имя: ")
+# age = input("Введите ваш возраст: ")
+# print("Привет," , name , " Тебе ",age ," лет")
+
+#Калькулятор
+value1 = int(input("Введите первую переменную:"))
+value2 = int(input("Введите вторую переменную:"))
+print(f"{value1} + {value2} = {value1 + value2}")#сумма элементов
+#string - тип данных для текста
+#int - тип данных для целочисленных чисел
+#float - тип данных с плавающей точкой
+#bool - тип данных логический, True, False
+print(f"{value1} - {value2} = {value1 - value2}")
+print(f"{value1} * {value2} = {value1 * value2}")
+print(f"{value1} / {value2} = {value1 / value2}")
+print(f"{value1} // {value2} = {value1 // value2}")
+print(f"{value1} % {value2} = {value1 % value2}")
+print(f"{value1} ** {value2} = {value1 ** value2}")
+print("*" * 11)
+#Проверка и сравнение данных
+print(value1 > value2)
+print(value1 < value2)
+print(value1 == value2)
+print(value1 == 'Hello World!!!') #Проверка на равенство
+print(value1 != 'Hello World!!!') #проверка на неравенство
+print("hello" > "hello world!!!")
+
+
+
+
